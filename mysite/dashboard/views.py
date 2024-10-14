29ad2b4747ca3c79aa6eb7aca6ba62db261ade13
Sty1ish <89575978+Sty1ish@users.noreply.github.com>
2024-10-14 17:52:10 +0900

    pie Chart 커밋
    pie Chart 커밋

--- mysite/dashboard/views.py
+++ mysite/dashboard/views.py
@@ -3,17 +3,7 @@ from django.http import HttpResponse
 import json
 from .models import *
 from collections import Counter
-
-# Create your views here.
-def index(request):
-    most_popular_ingredient = Counter([i.ingredient for i in Ingredient.objects.all()]).most_common(6)
-    most_popular_ingredient_name = [name for name, val in most_popular_ingredient]
-    most_popular_ingredient_amount = [val for name, val in most_popular_ingredient]
-    
-    context = {'most_popular_ingredient_name': json.dumps(most_popular_ingredient_name),
-               'most_popular_ingredient_amount' : json.dumps(most_popular_ingredient_amount),
-               }
-    return render(request, 'index.html', context)
+import pandas as pd
 
 def recipe_detail(request, recipe_id):
     recipe = Recipe.objects.get(id=recipe_id)
@@ -31,9 +21,19 @@ def recipe_list(request):
     top_ingredients = ingredient_counts.most_common(15)
     labels = [ingredient[0] for ingredient in top_ingredients]
     data = [ingredient[1] for ingredient in top_ingredients]
-
+    
+    # 요리 시간 구하기
+    cook_time = (pd.cut(pd.Series([i.time for i in Recipe.objects.all()]), [-1, 10,20,30,60,90,121])
+                 .value_counts()
+                 .sort_index()
+                 .reset_index()
+                 )
+    cook_time.loc[:, 'index'] = ['10분 미만', '10~20분', '20~30분', '30~60분', '60~90분', '90~120분']
+    
     return render(request, 'index.html', {
         'recipes': recipes,
         'labels': labels,
         'data': data,
+        'cook_time_label' : cook_time['index'].tolist(),
+        'cook_time_data' : cook_time['count'].tolist(),
     })
