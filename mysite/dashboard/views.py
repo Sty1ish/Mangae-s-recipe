1a8340138a584902a159686020d2f5b8473aeb3e
Sty1ish <89575978+Sty1ish@users.noreply.github.com>
2024-10-14 17:12:11 +0900

    레시피 사용 수 Plot
    레시피 사용 수 Plot

--- mysite/dashboard/views.py
+++ mysite/dashboard/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+import json
 from . import models
+from collections import Counter
 
 # Create your views here.
 def index(request):
-    latest_question_list = models.Recipe.objects.all()[0]
-    context = {'recipe': latest_question_list}
+    most_popular_ingredient = Counter([i.ingredient for i in models.Ingredient.objects.all()]).most_common(6)
+    most_popular_ingredient_name = [name for name, val in most_popular_ingredient]
+    most_popular_ingredient_amount = [val for name, val in most_popular_ingredient]
+    
+    context = {'most_popular_ingredient_name': json.dumps(most_popular_ingredient_name),
+               'most_popular_ingredient_amount' : json.dumps(most_popular_ingredient_amount),
+               }
     return render(request, 'index.html', context)
     
