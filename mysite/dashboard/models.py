a0a0c0e14f12426e6b72eb58335373cf06426ccd
Sty1ish <89575978+Sty1ish@users.noreply.github.com>
2024-10-14 10:40:55 +0900

    model 생성중
    - model 생성중

--- mysite/dashboard/models.py
+++ mysite/dashboard/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+class Recipe
+
+
 # Create your models here.
+class Recipe(models.Model):
+    recipe = models.CharField(max_length=200, verbose_name='레시피명')
+                                  
+    def __str__(self):
+        return f'레시피: {self.question_text}'
+
+class Ingredient(models.Model):
+    recipe = models.ForeignKey(Recipe, related_name='recipe', on_delete='확인필요')
+    ingredient = models.CharField(max_length=200, verbose_name='재료')
+    
+    def __str__(self):
+        return self.ingredient
+
+    
--- mysite/dashboard/views.py
+++ mysite/dashboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse 
 # Create your views here.
+def index(request):
+    # latest_question_list = models.Question.objects.order_by('-pub_date')[:5]
+    # context = {'questions': latest_question_list}
+    # #context = {'questions': []}
+    # return render(request, 'polls/index.html', context)
+    return HttpResponse('Hello')
