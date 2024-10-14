d557e780cf6522da9c9110965feee6262f964840
주호 <70103964+hosic2@users.noreply.github.com>
2024-10-14 11:25:55 +0900

    dashboard - models.py 수정
    dashboard - models.py 수정
    
    1. RecipeOrder, RecipeLink 추가
    2. realated_name 수정

--- mysite/dashboard/models.py
+++ mysite/dashboard/models.py
@@ -3,15 +3,27 @@ from django.db import models
 # Create your models here.
 class Recipe(models.Model):
     recipe_title = models.CharField(max_length=200, verbose_name='레시피명')
-                                  
+                                
     def __str__(self):
         return f'레시피: {self.recipe}'
 
 class Ingredient(models.Model):
-    recipe = models.ForeignKey(Recipe, related_name='recipe', on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, related_name='Ingredients', on_delete=models.CASCADE)
     ingredient = models.CharField(max_length=200, verbose_name='재료')
     
     def __str__(self):
         return self.ingredient
 
+class RecipeOrder(models.Model):
+    recipe = models.ForeignKey(Recipe, related_name='recipe orders', on_delete=models.CASCADE)
+    order = models.CharField(max_length=500, verbose_name='레시피 순서')
+
+    def __str__(self):
+        return f'{self.recipe} : {self.order}'
     
+class RecipeLink(models.Model):
+    recipe = models.ForeignKey(Recipe, related_name='recipe link', on_delete=models.CASCADE)
+    link = models.CharField(max_length=200, verbose_name='레시피 링크')
+
+    def __str__(self):
+        return self.link
