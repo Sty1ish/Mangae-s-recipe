c5357a1f1e0786ea839151bce2563c22507d21e4
Sty1ish <89575978+Sty1ish@users.noreply.github.com>
2024-10-14 10:13:58 +0900

    base data 크롤링
    월간 100위 데이터 크롤링 코드 작성

---
+++ crawl_data.py
@@ -0,0 +1,45 @@
+import requests
+from tqdm import tqdm
+import pandas as pd
+from bs4 import BeautifulSoup
+
+link = 'https://m.10000recipe.com/ranking/home_view.html?dtype=m&rtype=r'
+
+data = requests.get(link)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+line = soup.find_all('div', class_ = 'common_rcp_caption_tit line2')
+
+
+# data Save
+link_base = 'https://m.10000recipe.com/'
+
+df = pd.DataFrame(line, columns = ['data'])
+for idx, val in enumerate(line):
+    df.loc[idx, 'link'] = link_base + val['onclick'].replace("location.href='/",'').replace("'", '')
+
+
+
+
+
+# 개별 페이지 크롤링
+
+
+link = 'https://m.10000recipe.com/recipe/6845428'
+
+data = requests.get(link)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+list(map(lambda x : x.find('a').text.strip(), soup.find('dl', class_ = 'view3_ingre').find_all('li')))
+
+
+for idx in tqdm(range(len(df))):
+    link = df.loc[idx, 'link']
+    data = requests.get(link)
+    soup = BeautifulSoup(data.text, 'html.parser')
+    df.loc[idx, 'ingredient'] = str(list(map(lambda x : x.find('a').text.strip(), soup.find('dl', class_ = 'view3_ingre').find_all('li'))))
+
+
+df.to_csv('base_data.csv', index = False)
