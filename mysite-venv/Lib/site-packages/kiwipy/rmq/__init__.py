6b9b1729ed8bae16f06cc8f5ba581fe5b7508355
Sty1ish <89575978+Sty1ish@users.noreply.github.com>
2024-10-14 14:08:27 +0900

    gitignore 재적용

--- mysite-venv/Lib/site-packages/kiwipy/rmq/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# pylint: disable=cyclic-import,undefined-variable
-from .communicator import *
-from .tasks import *
-from .threadcomms import *
-from .utils import *
-
-__all__ = (tasks.__all__ + communicator.__all__ + threadcomms.__all__ + utils.__all__)
