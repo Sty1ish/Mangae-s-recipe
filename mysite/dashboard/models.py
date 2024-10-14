f76e7ffa67c59a394b19fb69ac6461d2312af7dd
주호 <70103964+hosic2@users.noreply.github.com>
2024-10-14 11:32:24 +0900

    dashboard - models.py 수정
    dashboard - models.py 수정
    
    1. 공백 수정

--- mysite/dashboard/models.py
+++ mysite/dashboard/models.py
@@ -15,14 +15,14 @@ class Ingredient(models.Model):
         return self.ingredient
 
 class RecipeOrder(models.Model):
-    recipe = models.ForeignKey(Recipe, related_name='recipe orders', on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, related_name='recipe_orders', on_delete=models.CASCADE)
     order = models.CharField(max_length=500, verbose_name='레시피 순서')
 
     def __str__(self):
         return f'{self.recipe} : {self.order}'
     
 class RecipeLink(models.Model):
-    recipe = models.ForeignKey(Recipe, related_name='recipe link', on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, related_name='recipe_link', on_delete=models.CASCADE)
     link = models.CharField(max_length=200, verbose_name='레시피 링크')
 
     def __str__(self):
