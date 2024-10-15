1932d4fa73ffb51973ceae7290bb27fcc4df193c
주호 <70103964+hosic2@users.noreply.github.com>
2024-10-15 11:22:01 +0900

    visual_detail 페이지 추가
    visual_detail 페이지 추가

--- mysite/dashboard/views.py
+++ mysite/dashboard/views.py
@@ -6,15 +6,36 @@ from collections import Counter
 import json
 import pandas as pd
 
-def index(request):
-    most_popular_ingredient = Counter([i.ingredient for i in Ingredient.objects.all()]).most_common(6)
-    most_popular_ingredient_name = [name for name, val in most_popular_ingredient]
-    most_popular_ingredient_amount = [val for name, val in most_popular_ingredient]
+def visual_detail(request):
+    recipes = Recipe.objects.all()
     
-    context = {'most_popular_ingredient_name': json.dumps(most_popular_ingredient_name),
-               'most_popular_ingredient_amount' : json.dumps(most_popular_ingredient_amount),
-               }
-    return render(request, 'index.html', context)
+    ingredients = Ingredient.objects.all()
+    ingredient_counts = Counter(ingredient.ingredient for ingredient in ingredients)
+
+    top_ingredients = ingredient_counts.most_common()
+    labels = [ingredient[0] for ingredient in top_ingredients]
+    data = [ingredient[1] for ingredient in top_ingredients]
+
+    # 요리 시간 구하기
+    cook_time = (pd.cut(pd.Series([i.time for i in Recipe.objects.all()]), [-1, 10, 20, 30, 60, 90, 121])
+                 .value_counts()
+                 .sort_index()
+                 .reset_index()
+                 )
+    cook_time.loc[:, 'index'] = ['10분 미만', '10~20분', '20~30분', '30~60분', '60~90분', '90~120분']
+    
+    # 랜덤 레시피 선택
+    random_recipe = random.choice(recipes) if recipes else None
+
+    return render(request, 'visual_detail.html', {
+        'recipes': recipes,
+        'random_recipe': random_recipe,
+        'labels_json': json.dumps(labels, ensure_ascii=False),
+        'data_json': json.dumps(data),
+        'ingredients_json': json.dumps([{'name': name, 'frequency': freq} for name, freq in top_ingredients], ensure_ascii=False),
+        'cook_time_label': cook_time['index'].tolist(),
+        'cook_time_data': cook_time['count'].tolist(),
+    })
 
 def recipe_detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, id=recipe_id)
